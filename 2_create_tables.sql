6a6524591c837c8cdf0415e2ed75bc367f297d8a
wadjih-bencheikh18 <jw_bencheikh@esi.dz>
2023-01-21 23:02:11 +0100

    fix code

--- 2_create_tables.sql
+++ 2_create_tables.sql
@@ -1,48 +1,45 @@
-
 -- Drop tables
-DROP TABLE publication_authors;
-DROP TABLE authors;
-DROP TABLE ee;
-DROP TABLE publications;
+-- DROP TABLE publication_authors;
+-- DROP TABLE authors;
+-- DROP TABLE ee;
+-- DROP TABLE publications;
 
 -- Create tables
-CREATE TABLE publications (
-  id INT PRIMARY KEY,
-  type VARCHAR(255),
-  title VARCHAR(255),
-  pages VARCHAR(30),
-  year INT,
-  booktitle VARCHAR(255),
-  crossref VARCHAR(255),
-  school VARCHAR(255),
-  volume INT,
-  journal VARCHAR(255),
-  num INT,
-  url VARCHAR(255)
+CREATE TABLE PUBLICATIONS (
+  ID INT PRIMARY KEY,
+  TYPE VARCHAR(255),
+  TITLE VARCHAR(255),
+  PAGES VARCHAR(30),
+  YEAR INT,
+  BOOKTITLE VARCHAR(255),
+  CROSSREF VARCHAR(255),
+  SCHOOL VARCHAR(255),
+  VOLUME INT,
+  JOURNAL VARCHAR(255),
+  NUM INT,
+  URL VARCHAR(255)
 );
 
-CREATE TABLE ee (
-  id INT PRIMARY KEY,
-  url VARCHAR(255),
-  publication_id INT,
-  FOREIGN KEY (publication_id) REFERENCES publications(id)
+CREATE TABLE EE (
+  ID INT PRIMARY KEY,
+  URL VARCHAR(255),
+  PUBLICATION_ID INT,
+  FOREIGN KEY (PUBLICATION_ID) REFERENCES PUBLICATIONS(ID)
 );
 
-CREATE TABLE authors (
-  id INT PRIMARY KEY,
-  name VARCHAR(255)
+CREATE TABLE AUTHORS (
+  ID INT PRIMARY KEY,
+  NAME VARCHAR(255)
 );
 
-CREATE TABLE publication_authors (
-  id INT PRIMARY KEY,
-  publication_id INT,
-  author_id INT,
-  FOREIGN KEY (publication_id) REFERENCES publications(id),
-  FOREIGN KEY (author_id) REFERENCES authors(id)
+CREATE TABLE PUBLICATION_AUTHORS (
+  ID INT PRIMARY KEY,
+  PUBLICATION_ID INT,
+  AUTHOR_ID INT,
+  FOREIGN KEY (PUBLICATION_ID) REFERENCES PUBLICATIONS(ID),
+  FOREIGN KEY (AUTHOR_ID) REFERENCES AUTHORS(ID)
 );
-
-
 
 -- Insert data
 
--- 
+--
--- 4_objet_relationnelle.sql
+++ 4_objet_relationnelle.sql
@@ -1,36 +1,84 @@
-
 -- Liste de tous les livres (type "Book") :
-SELECT * FROM publications WHERE type = 'Book';
+SELECT
+    *
+FROM
+    PUBLICATIONS
+WHERE
+    TYPE = 'Book';
 
 -- Types des publications de l’an 2022 :
-SELECT type, COUNT(*) FROM publications WHERE year = 2022 GROUP BY type;
+SELECT
+    TYPE,
+    COUNT(*)
+FROM
+    PUBLICATIONS
+WHERE
+    YEAR = 2022
+GROUP BY
+    TYPE;
 
 -- Liste des journaux depuis 2008 :
-SELECT * FROM publications WHERE type = 'Journal' AND year >= 2008;
+SELECT
+    *
+FROM
+    PUBLICATIONS
+WHERE
+    TYPE = 'Journal'
+    AND YEAR >= 2008;
 
 -- Liste des publications de l’auteur « Yacine Mestoui » :
-SELECT p.* FROM publications p
-INNER JOIN publication_authors pa ON p.id = pa.publication_id
-INNER JOIN authors a ON pa.author_id = a.id
-WHERE a.name = 'Yacine Mestoui';
+SELECT
+    P.*
+FROM
+    PUBLICATIONS        P
+    INNER JOIN PUBLICATION_AUTHORS PA
+    ON P.ID = PA.PUBLICATION_ID
+    INNER JOIN AUTHORS A
+    ON PA.AUTHOR_ID = A.ID
+WHERE
+    A.NAME = 'Yacine Mestoui';
 
 -- Liste de tous les articles publiés à DaWaK :
-SELECT * FROM publications WHERE booktitle = 'DaWaK';
+SELECT
+    *
+FROM
+    PUBLICATIONS
+WHERE
+    BOOKTITLE = 'DaWaK';
 
 -- Liste de tous les auteurs distincts :
-SELECT DISTINCT name FROM authors;
+SELECT
+    DISTINCT NAME
+FROM
+    AUTHORS;
 
 -- Trier les publications de « Alfredo Cuzzocrea » par année :
-SELECT p.* FROM publications p
-INNER JOIN publication_authors pa ON p.id = pa.publication_id
-INNER JOIN authors a ON pa.author_id = a.id
-WHERE a.name = 'Alfredo Cuzzocrea'
-ORDER BY p.year;
+SELECT
+    P.*
+FROM
+    PUBLICATIONS        P
+    INNER JOIN PUBLICATION_AUTHORS PA
+    ON P.ID = PA.PUBLICATION_ID
+    INNER JOIN AUTHORS A
+    ON PA.AUTHOR_ID = A.ID
+WHERE
+    A.NAME = 'Alfredo Cuzzocrea'
+ORDER BY
+    P.YEAR;
 
 -- Corriger le nom de « Carlos Ordonez 0001 » par « Carlos Ordonez» :
-UPDATE authors SET name = 'Carlos Ordonez' WHERE name = 'Carlos Ordonez 0001';
+UPDATE AUTHORS
+SET
+    NAME = 'Carlos Ordonez'
+WHERE
+    NAME = 'Carlos Ordonez 0001';
 
 -- Compter le nombre des publications de « Soumia Benkrid» :
-SELECT COUNT(*) FROM publication_authors pa
-INNER JOIN authors a ON pa.author_id = a.id
-WHERE a.name = 'Soumia Benkrid';
+SELECT
+    COUNT(*)
+FROM
+    PUBLICATION_AUTHORS PA
+    INNER JOIN AUTHORS A
+    ON PA.AUTHOR_ID = A.ID
+WHERE
+    A.NAME = 'Soumia Benkrid';
